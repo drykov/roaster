373e29c329e1c974ba1a103c3b8a31cd99bcac7a
Dmitry Rykov <rykov@reksoft.com>
2022-08-10 16:23:29 +0300

    Initial Commit

---
+++ roaster/roasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  roasterApp.swift
+//  roaster
+//
+//  Created by Dmitry Rykov on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct roasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
